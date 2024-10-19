e289af3393f035b369c484483ae723e61e0df27a
Kristian Weegink <k.weegink@gmail.com>
2024-10-19 21:57:59 +1000

    random mant reqs

--- random_math/circle_area.py
+++ random_math/circle_area.py
@@ -14,19 +14,21 @@ x2 = math.cos(theta_2)
 # circle and integrate form x2 to x1. y = sqrt(r**2 - x**2)
 
 x = sympy.Symbol('x')
-r = 1
+r = 6
 int_x = (sympy.integrate(sympy.sqrt(r**2 - x**2), x))
 # x*sqrt(1 - x**2)/2 + asin(x)/2
 theta_1 = sympy.pi/6
 theta_2 = sympy.pi/3
-x1 = math.cos(theta_1)
-x2 = math.cos(theta_2)
+x1 = r*math.cos(theta_1)
+x2 = r*math.cos(theta_2)
 area = int_x.subs({"x":x1}) - int_x.subs({"x":x2})
 
 print(area)
 
 area = sympy.integrate(sympy.sqrt(r**2 - x**2), (x, x2, x1))
 print(area)
+
+print(3 * math.pi)
 
 
 # Double integral in polar coordinates. Integrate from 0 to theta_1 and 
